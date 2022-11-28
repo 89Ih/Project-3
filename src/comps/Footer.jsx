59fe611294cd8b7eab880bbfd1c9e295e60963ca
Enrique Dapelo <enricodapelo6@hotmail.com>
2022-11-28 17:43:25 +0100

    log out function

--- src/comps/Footer.jsx
+++ src/comps/Footer.jsx
@@ -1,30 +1,34 @@
-import circle from '../icons/circle.png'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { useContext } from 'react'
-import { SessionContext } from '../contexts/SessionContext'
-
+import circle from "../icons/circle.png";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useContext } from "react";
+import { SessionContext } from "../contexts/SessionContext";
+import { useState } from "react";
 
 const Footer = () => {
+  const { isAuthenticated, token } = useContext(SessionContext);
+  const [isLoggedin, setIsLoggedin] = useState(false);
+  const logout = () => {
+    localStorage.clear();
+    setIsLoggedin(false);
+  };
+  return (
+    <footer
+      className="css d-flex justify-content-end align-items-center p-1"
+      style={{ gap: 5 }}
+    >
+      <b>
+        <a className="nav-link text-light  " href="/login" onClick={logout}>
+          LOGOUT
+        </a>
+      </b>
 
-     const { isAuthenticated } = useContext(SessionContext)
-   
-     return (
-      
-        <footer className="css d-flex justify-content-end align-items-center p-1" style={{gap:5}}>        
-        
-        <b><a className="nav-link text-light  " href="/">Sign out</a></b>
-        
-        {isAuthenticated ? (
-    
-       <img src= {circle} alt={'circle'} className='c-success   '/>
-
+      {isAuthenticated ? (
+        <img src={circle} alt={"circle"} className="c-success   " />
       ) : (
-       
-        <img src= {circle} alt={'circle'} className='c-danger'/>
+        <img src={circle} alt={"circle"} className="c-danger" />
       )}
-        
-        </footer>
-      );
-}
- 
+    </footer>
+  );
+};
+
 export default Footer;
