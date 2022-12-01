af278bd24216895c5c98fe48bd969284df0c02e5
Enrique Dapelo <enricodapelo6@hotmail.com>
2022-12-01 20:46:10 +0100

    plan number

--- src/comps/Rating.jsx
+++ src/comps/Rating.jsx
@@ -1,54 +1,60 @@
 import { useParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-import { useState,useContext,useEffect } from "react";
+import { useState, useContext, useEffect } from "react";
 import { SessionContext } from "../contexts/SessionContext";
-import axios from 'axios';
+import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Stars from "./Stars";
 const Rating = () => {
-    const params = useParams();
-    const [comment, setComment] = useState("");
-    const [rating, setRating] = useState(0);
+  const params = useParams();
+  const [comment, setComment] = useState("");
+  const [rating, setRating] = useState(0);
 
-    const { user, token } = useContext(SessionContext);
-    const handleSubmit = async event => {
-        event.preventDefault()
-        const response = await fetch(`${process.env.REACT_APP_API_URL}course/${params.id}`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${token}`,
-          },
-          body: JSON.stringify({  comment,rating, id: user._id }),
-        })
-        const parsed = await response.json()
-        console.log(parsed)
-       
+  const { user, token } = useContext(SessionContext);
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const response = await fetch(
+      `${process.env.REACT_APP_API_URL}course/${params.id}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({ comment, rating, id: user._id }),
       }
-      const[rate,setRate] = useState({})
-      useEffect(()=> {
-        const retrieveRating = async()=>{
-            const res = await axios.get(`${process.env.REACT_APP_API_URL}rating/${params.id}`);
-            console.log("Rating",res.data)
-            setRate(res.data);
-        }
-        retrieveRating()
-    },[params.id]); 
-
-    const navigate = useNavigate();
-    const deleteRating= async (event) => {
-      event.preventDefault();
-      const res = await axios.delete(`${process.env.REACT_APP_API_URL}rating/${params.id}`, params.id);
-      console.log(res.data);
-      // navigate(`/courses/${course._id}`);
+    );
+    const parsed = await response.json();
+    console.log(parsed);
+  };
+  const [rate, setRate] = useState({});
+  useEffect(() => {
+    const retrieveRating = async () => {
+      const res = await axios.get(
+        `${process.env.REACT_APP_API_URL}rating/${params.id}`
+      );
+      console.log("Rating", res.data);
+      setRate(res.data);
     };
+    retrieveRating();
+  }, [params.id]);
 
-    return ( <div>
+  const navigate = useNavigate();
+  const deleteRating = async (event) => {
+    event.preventDefault();
+    const res = await axios.delete(
+      `${process.env.REACT_APP_API_URL}rating/${params.id}`,
+      params.id
+    );
+    console.log(res.data);
+    // navigate(`/courses/${course._id}`);
+  };
 
-      <div className="p-2" style={{ width: 700}}>
-    
-        <div className="card-body d-flex flex-column m-3"  style={{ gap: 10}}>
-        {/* {rate.rate?.comment === null && rate.rate?.rating === null? (
+  return (
+    <div>
+      <div className="p-2" style={{ width: 700 }}>
+        <div className="card-body d-flex flex-column m-3" style={{ gap: 10 }}>
+          {/* {rate.rate?.comment === null && rate.rate?.rating === null? (
         <div className="card-title d-flex flex-row rounded border  bg-white " >
         <p className="card-text flex-grow-1 m-2">{rate.rate?.comment} </p>
         <p className="card-text mx-4 m-2"><Stars>{rate.rate?.rating}</Stars></p>
@@ -57,11 +63,19 @@ const Rating = () => {
       ) : (
        <p></p>
       )} */}
-          <div className="card-title d-flex flex-row rounded border  bg-white " >
+          <div className="card-title d-flex flex-row rounded border  bg-white ">
             <p className="card-text flex-grow-1 m-2">{rate.rate?.comment} </p>
-            <p className="card-text mx-4 m-2"><Stars>{rate.rate?.rating}</Stars></p>
-            <button onClick={deleteRating} className=" css css-rating-box" style={{ border: "none" }}>Delete</button>
-            </div>
+            <p className="card-text mx-4 m-2">
+              <Stars>{rate.rate?.rating}</Stars>
+            </p>
+            <button
+              onClick={deleteRating}
+              className=" css css-rating-box"
+              style={{ border: "none" }}
+            >
+              Delete
+            </button>
+          </div>
 
           <form onSubmit={handleSubmit} className="">
             <div className="form-group  d-flex rounded border  ">
@@ -75,7 +89,7 @@ const Rating = () => {
                 className="form-control"
                 style={{ border: "none" }}
               />
-                <input
+              <input
                 type="number"
                 value={rating}
                 placeholder="rate a course"
@@ -83,19 +97,20 @@ const Rating = () => {
                 max="5"
                 onChange={(event) => setRating(event.target.value)}
                 style={{ border: "none" }}
-                
               />
-               <button type="submit" className=" css css-rating-box" style={{ border: "none" }}>
+              <button
+                type="submit"
+                className=" css css-rating-box"
+                style={{ border: "none" }}
+              >
                 Create
               </button>
             </div>
-           
-
-            
-      </form>
+          </form>
         </div>
       </div>
-    </div> );
-}
- 
+    </div>
+  );
+};
+
 export default Rating;
--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -1,88 +1,99 @@
 import { SessionContext } from "../contexts/SessionContext";
 import { useContext } from "react";
 import { useEffect, useState } from "react";
-import axios from 'axios';
+import axios from "axios";
 import { Link } from "react-router-dom";
 import Header from "../comps/Header";
 import Footer from "../comps/Footer";
-import Sort from '../icons/sort.png';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Card, Row, Col} from 'antd';
+import Sort from "../icons/sort.png";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Card, Row, Col } from "antd";
 
 const Courses = () => {
-   
-    const[courses,setCourses] = useState([])
-    const[query,setQuery] = useState("")
-   
-    useEffect(()=> {
-    
-        const retrieveCourses = async()=>{
-    
-            const res = await axios.get(`${process.env.REACT_APP_API_URL}courses`);
-    
-            console.log(res.data)
-    
-            setCourses(res.data);
-        }
-        
-        retrieveCourses()
-    },[]); 
+  const [courses, setCourses] = useState([]);
+  const [query, setQuery] = useState("");
 
+  useEffect(() => {
+    const retrieveCourses = async () => {
+      const res = await axios.get(`${process.env.REACT_APP_API_URL}courses`);
 
-    
-    
-    const sortName = () => {
-        
-        const courseSorted = Array.from(courses).sort((a, b) =>a.title.localeCompare(b.title)); 
-        
-        setCourses(courseSorted);
-      };
+      console.log(res.data);
 
-     
+      setCourses(res.data);
+    };
 
-      
-    return (
-    <div className='App-body'>
-  
+    retrieveCourses();
+  }, []);
 
-            <div className=" d-flex flex-column " style={{ gap: 10 }}>
-                <Header />
-              
-                <div className=" w-50 align-self-center align-items-center" >
-                    <div className=" d-flex flex-row   " >
-                        <input className="form-control" type="text" placeholder="Search" value={query} onChange={(event) => { setQuery(event.target.value) }} />
-                        <img onClick={sortName} src={Sort} alt={'sort'} className="css-sort" style={{width:30}} />
-                    </div>
-                    <h5  className="d-flex justify-content-center m-1">your plan contains <em className="css-color">{courses.length}</em> courses</h5>
-                </div>
+  const sortName = () => {
+    const courseSorted = Array.from(courses).sort((a, b) =>
+      a.title.localeCompare(b.title)
+    );
 
-                <Row style={{ width: '100%', justifyContent: 'center' }}>
-                    {courses.filter((course) => course.title.toLowerCase().includes(query.toLowerCase())
-                    ).map((course) => (<Col key={course._id}>
-                        <Card style={{ width: 400, height:450, margin: 10 }}>
-                        <Link to={`/course/${course._id}`} >
-                            <img src={course.image} width={350} height={150} alt={course.title} className="rounded border" />
-                        </Link>
-                            <p>{course.title}</p>
-                            <p>{course.description}</p>
-                            <p><b>{course.price} </b> € </p>
-                        </Card>
-    
-                       
-                    </Col>))}
-                </Row>
+    setCourses(courseSorted);
+  };
 
+  return (
+    <div className="App-body">
+      <div className=" d-flex flex-column " style={{ gap: 10 }}>
+        <Header />
 
-            </div>
+        <div className=" w-50 align-self-center align-items-center">
+          <div className=" d-flex flex-row   ">
+            <input
+              className="form-control"
+              type="text"
+              placeholder="Search"
+              value={query}
+              onChange={(event) => {
+                setQuery(event.target.value);
+              }}
+            />
+            <img
+              onClick={sortName}
+              src={Sort}
+              alt={"sort"}
+              className="css-sort"
+              style={{ width: 30 }}
+            />
+          </div>
+          <h5 className="d-flex justify-content-center m-1">
+            Your plan contains <em className="css-color">{courses.length}</em>{" "}
+            courses
+          </h5>
+        </div>
 
-                       
+        <Row style={{ width: "100%", justifyContent: "center" }}>
+          {courses
+            .filter((course) =>
+              course.title.toLowerCase().includes(query.toLowerCase())
+            )
+            .map((course) => (
+              <Col key={course._id}>
+                <Card style={{ width: 400, height: 450, margin: 10 }}>
+                  <Link to={`/course/${course._id}`}>
+                    <img
+                      src={course.image}
+                      width={350}
+                      height={150}
+                      alt={course.title}
+                      className="rounded border"
+                    />
+                  </Link>
+                  <p>{course.title}</p>
+                  <p>{course.description}</p>
+                  <p>
+                    <b>{course.price} </b> €{" "}
+                  </p>
+                </Card>
+              </Col>
+            ))}
+        </Row>
+      </div>
 
-  <Footer/>
+      <Footer />
+    </div>
+  );
+};
 
-
-
-  
-      </div> );
-}
- 
 export default Courses;
